175de3a70e5fe09c464fcaf6ba6c5beccf5f4daa
Penelope Drago <prd@drago.com.au>
2023-03-07 15:16:52 +1100

    add functions

---
+++ index.js
@@ -0,0 +1,47 @@
+// Addition function
+
+function add(a, b) {
+  return a + b;
+}
+
+// Subtraction function
+
+function subtract(a, b) {
+  return a - b;
+}
+
+// Multiplication function
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// Division function
+
+function divide(a, b) {
+  return a / b;
+}
+
+// Function to increment by 1
+
+function increment(n) {
+  return n + 1;
+}
+
+// Function to decrement by 1
+
+function decrement(n) {
+  return n - 1;
+}
+
+// Function to parse string and return integer
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+// Function to parse string and return float
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
